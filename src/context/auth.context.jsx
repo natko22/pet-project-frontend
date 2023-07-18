24f88dcd24bfe89c50740079bb87cc223f513298
Natassa <112552395+natko22@users.noreply.github.com>
2023-07-18 21:54:58 +0200

    add url

--- src/context/auth.context.jsx
+++ src/context/auth.context.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5005";
+import { API_URL } from "../config/config.index";
 
 const AuthContext = React.createContext();
 
